e67bf87add81b41d055c2771b7f917a5ec4bbd10
Shourya Singh <imshourya07singh@gmail.com>
2025-07-09 06:02:26 +0530

    Prepare for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-  },
-  preview: {
-    port: 5173,
-    strictPort: true,
-    host: true,
-    origin: 'http://0.0.0.0:5173',
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-  },
-  base: './',
 })
